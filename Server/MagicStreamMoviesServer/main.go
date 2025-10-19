10cb29ac92d8111e9df2001fec2f645f737126b6
Vijayakumar-Harish <vijayakumarharisha@gmail.com>
2025-10-19 12:06:19 +0530

    fix: cors and url resolve

--- Server/MagicStreamMoviesServer/main.go
+++ Server/MagicStreamMoviesServer/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/gin-gonic/gin"
+	"log"
+	"strings"
+	"time"
+	"os"
 	routes "github.com/Vijayakumar-Harish/MagicStreamMovies/Server/MagicStreamMoviesServer/routes"
+	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 
 	database "github.com/Vijayakumar-Harish/MagicStreamMovies/Server/MagicStreamMoviesServer/database"
-
+	"github.com/gin-contrib/cors"
 )
 
 func main() {
@@ -18,6 +21,31 @@ func main() {
 	router.GET("/hello", func(c *gin.Context) {
 		c.String(200, "Hello, MagicStreamMovies!")
 	})
+
+	allowedOrigins := os.Getenv("ALLOWED_ORIGINS")
+
+	var origins []string
+	if allowedOrigins != "" {
+		origins = strings.Split(allowedOrigins, ",")
+		for i := range origins {
+			origins[i] = strings.TrimSpace(origins[i])
+			log.Println("Allowed Origin:", origins[i])
+		}
+	} else {
+		origins = []string{"http://localhost:5173"}
+		log.Println("Allowed Origin: http://localhost:5173")
+	}
+
+	config := cors.Config{}
+	config.AllowOrigins = origins
+	config.AllowMethods = []string{"GET","POST","PATCH","PUT","DELETE","OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Type","Authorization"}
+	config.ExposeHeaders = []string{"Content-Length"}
+	config.AllowCredentials = true
+	config.MaxAge = 12 * time.Hour
+
+	router.Use(cors.New(config))
+	router.Use(gin.Logger())
 
 	var client *mongo.Client = database.Connect()
 
